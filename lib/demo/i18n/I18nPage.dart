15e34dc68a957f8025142e5cfb4476726c4e1137
jeek <gionchat@gmail.com>
2020-12-21 15:57:30 +0800

    1.Stream  Stream StreamController StreamSink StreamSubscription

--- lib/demo/i18n/I18nPage.dart
+++ lib/demo/i18n/I18nPage.dart
@@ -1,48 +1,47 @@
 /**
  * Copyright (C), 2015-2020, suntront
- * FileName: RXDartPage
+ * FileName: I18nPage
  * Author: Jeek
- * Date: 2020/12/1
+ * Date: 2020/12/21
  * Description:
  */
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(RXDartPage());
+void main() => runApp(I18nPage());
 
-class RXDartPage extends StatelessWidget {
+class I18nPage extends StatelessWidget {
   final SystemUiOverlayStyle _style = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(_style);
     // TODO: implement build
     return MaterialApp(
-      home: RXDartPageStatefulWidget(),
+      home: I18nPageStatefulWidget(),
       theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
 
-class RXDartPageStatefulWidget extends StatefulWidget {
+class I18nPageStatefulWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return _RXDartPageState();
+    return _I18nPageState();
   }
 }
 
-class _RXDartPageState extends State<StatefulWidget> {
+class _I18nPageState extends State<StatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("RXDartPage"),
+        title: Text("I18nPage"),
       ),
       body: Center(
-        child: Text("RXDartPage"),
+        child: Text("I18nPage"),
       ),
     );
   }
--- lib/demo/stream/StreamPage.dart
+++ lib/demo/stream/StreamPage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 /**
  * Copyright (C), 2015-2020, suntront
  * FileName: StreamPage
@@ -5,14 +7,17 @@
  * Date: 2020/12/1
  * Description:
  */
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_ninghao/base/utils/LogUtils.dart';
 
 void main() => runApp(StreamPage());
+final String TAG = "StreamPage";
 
 class StreamPage extends StatelessWidget {
-  final SystemUiOverlayStyle _style = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+  final SystemUiOverlayStyle _style =
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(_style);
@@ -33,6 +38,38 @@ class StreamPageStatefulWidget extends StatefulWidget {
 }
 
 class _StreamPageState extends State<StatefulWidget> {
+  StreamSubscription streamSubscription = null;
+  StreamController<String> streamController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    LogUtils.i(TAG, "jeek create Stream");
+    //Stream<String> stream = Stream.fromFuture(fetchData());
+    streamController = StreamController<String>();
+    LogUtils.i(TAG, "jeek listen Stream");
+    streamSubscription =
+        streamController.stream.listen(onData, onError: onError, onDone: onDone);
+    LogUtils.i(TAG, "jeek init complete");
+  }
+
+  void onData(String data) {
+    LogUtils.i(TAG, "jeek stream.listen onData: $data");
+  }
+
+  void onDone() {
+    LogUtils.i(TAG, "jeek stream.listen onDone");
+  }
+
+  void onError(error) {
+    LogUtils.i(TAG, "jeek stream.listen onError: $error");
+  }
+
+  Future<String> fetchData() async {
+    await Future.delayed(Duration(seconds: 3));
+    return "Hello Word";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +79,32 @@ class _StreamPageState extends State<StatefulWidget> {
         title: Text("StreamPage"),
       ),
       body: Center(
-        child: Text("StreamPage"),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlineButton(
+              onPressed: () {
+                LogUtils.i(TAG, "jeek StreamSubscription Pause");
+              },
+              child: Text("Pause"),
+            ),
+
+            OutlineButton(
+              onPressed: () {
+                LogUtils.i(TAG, "jeek StreamSubscription Resume");
+              },
+              child: Text("Resume"),
+            ),
+
+
+            OutlineButton(
+              onPressed: () {
+                LogUtils.i(TAG, "jeek StreamSubscription Cancel");
+              },
+              child: Text("Cancel"),
+            ),
+          ],
+        ),
       ),
     );
   }
